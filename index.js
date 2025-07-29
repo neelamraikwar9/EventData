e2aa7fdb218800ed7857d5b15e518fd8525f97df
Neelam Raikwar <neelam.raikwar.234303@gmail.com>
2025-07-29 16:35:15 +0530

    fix: fixed index file.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { initializeDB } = require("./db.connect");
-const fs = require("fs");
+// const fs = require("fs");
 const Meet = require("./meet.model");
 
 
@@ -17,40 +17,40 @@ const corsOptions = {
 
 initializeDB();
 
-const jsonData = fs.readFileSync("meet.json", "utf-8");
-console.log(jsonData)
-const meetsData = JSON.parse(jsonData);
-console.log(meetsData)
+// const jsonData = fs.readFileSync("meet.json", "utf-8");
+// console.log(jsonData)
+// const meetsData = JSON.parse(jsonData);
+// console.log(meetsData)
 
-function seedData(){
-    try{
-        for(const meetData of meetsData){
-            const newMeet = new Meet({
-                eventTitle: meetData.title,
-                eventHost: meetData.host,
-                eventDate: meetData.date,
-                deventDay: meetData.day,
-                eventBeginingTime: meetData.beginingTime,
-                eventEndingTime: meetData.endingTime,
-                eventMode: meetData.mode,
-                eventAddress: meetData.address,
-                eventCity: meetData.city,
-                eventDetail: meetData.detail,
-                eventDressCode: meetData.dressCode,
-                eventRestriction: meetData.restriction,
-                eventWebsite: meetData.website,
-                eventPrice:  meetData.price,
-                eventImages: meetData.images
-            });
+// function seedData(){
+//     try{
+//         for(const meetData of meetsData){
+//             const newMeet = new Meet({
+//                 eventTitle: meetData.title,
+//                 eventHost: meetData.host,
+//                 eventDate: meetData.date,
+//                 deventDay: meetData.day,
+//                 eventBeginingTime: meetData.beginingTime,
+//                 eventEndingTime: meetData.endingTime,
+//                 eventMode: meetData.mode,
+//                 eventAddress: meetData.address,
+//                 eventCity: meetData.city,
+//                 eventDetail: meetData.detail,
+//                 eventDressCode: meetData.dressCode,
+//                 eventRestriction: meetData.restriction,
+//                 eventWebsite: meetData.website,
+//                 eventPrice:  meetData.price,
+//                 eventImages: meetData.images
+//             });
 
-            // console.log(newMeet.eventTitle)
-            newMeet.save();
-        }
+//             // console.log(newMeet.eventTitle)
+//             newMeet.save();
+//         }
 
-    } catch(error){
-        console.log("Error in seeding the data", error);
-    }
-}
+//     } catch(error){
+//         console.log("Error in seeding the data", error);
+//     }
+// }
 
 // seedData();
 
